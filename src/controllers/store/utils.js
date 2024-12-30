248507ed8ef002814161ec31ff8802a10f69a548
Raphico <raphicogit@gmail.com>
2024-12-31 00:03:11 +0100

    refactor: store images upload

--- src/controllers/store/utils.js
+++ src/controllers/store/utils.js
@@ -3,39 +3,45 @@ import { v2 as cloudinary } from "cloudinary";
 import { uploadFile } from "../../services/cloudinary.service.js";
 
 export async function uploadStoreImages(request, prevBannerUrl, prevLogoUrl) {
-    let bannerUrl;
+    const uploadTasks = [];
+
     if (request.files?.banner) {
         const readable = stream.Readable.from(request.files.banner[0].buffer);
-        const uploadResult = await uploadFile(readable);
-
-        if (prevBannerUrl) {
-            await cloudinary.uploader.destroy(
-                extractPublicIdFromUrl(prevBannerUrl)
-            );
-        }
-
-        bannerUrl = cloudinary.url(uploadResult.public_id, {
-            fetch_format: "auto",
-            quality: "auto",
-        });
+        const bannerUploadTask = uploadFile(readable).then(
+            async (uploadResult) => {
+                if (prevBannerUrl) {
+                    await cloudinary.uploader.destroy(
+                        extractPublicIdFromUrl(prevBannerUrl)
+                    );
+                }
+                return cloudinary.url(uploadResult.public_id, {
+                    fetch_format: "auto",
+                    quality: "auto",
+                });
+            }
+        );
+        uploadTasks.push(bannerUploadTask);
     }
 
-    let logoUrl;
     if (request.files?.logo) {
         const readable = stream.Readable.from(request.files.logo[0].buffer);
-        const uploadResult = await uploadFile(readable);
-
-        if (prevLogoUrl) {
-            await cloudinary.uploader.destroy(
-                extractPublicIdFromUrl(prevLogoUrl)
-            );
-        }
-
-        logoUrl = cloudinary.url(uploadResult.public_id, {
-            fetch_format: "auto",
-            quality: "auto",
-        });
+        const logoUploadTask = uploadFile(readable).then(
+            async (uploadResult) => {
+                if (prevLogoUrl) {
+                    await cloudinary.uploader.destroy(
+                        extractPublicIdFromUrl(prevLogoUrl)
+                    );
+                }
+                return cloudinary.url(uploadResult.public_id, {
+                    fetch_format: "auto",
+                    quality: "auto",
+                });
+            }
+        );
+        uploadTasks.push(logoUploadTask);
     }
+
+    const [bannerUrl, logoUrl] = await Promise.all(uploadTasks);
 
     return {
         logoUrl,
